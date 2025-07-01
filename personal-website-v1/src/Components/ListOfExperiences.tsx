5d9a2a0c6b1b5d3b01a0744961c69eb66cac2bc3
Maharshi Manubrolu <maharshimanubrolu23@gmail.com>
2025-07-02 00:41:48 +0530

    adding experience split section

---
+++ personal-website-v1/src/Components/ListOfExperiences.tsx
@@ -0,0 +1,56 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+function ListOfExperiences(props) {
+  const { experiences, setContent } = props;
+  const [selectedIndex, setSelectedIndex] = React.useState<number | null>(null);
+  return (
+    <Box
+      sx={{
+        color: "white",
+      }}
+    >
+      {experiences?.map((item: any, index: number) => {
+        return (
+          <Box
+            key={item?.role + index}
+            onClick={() => {
+              setContent(item?.richDescription);
+              setSelectedIndex(index);
+            }}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              padding: "1vw",
+              marginBottom: "1.5vw",
+              border:
+                selectedIndex === index
+                  ? "1px solid rgba(230, 149, 235, 0.1)"
+                  : "unset",
+            }}
+          >
+            <Typography>{item?.role}</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "1vw",
+                alignItems: "center",
+                marginTop: "1vw",
+              }}
+            >
+              <Typography>{` From ${item?.startDate} - `}</Typography>
+              {item?.present ? (
+                <Typography>Present</Typography>
+              ) : (
+                <Typography>{item?.endDate}</Typography>
+              )}
+            </Box>
+          </Box>
+        );
+      })}
+    </Box>
+  );
+}
+
+export default ListOfExperiences;
